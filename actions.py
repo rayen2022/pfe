b5253740db18012a4ad7028b2906ad6a60b1abdc
chatbot <67395892+rayen2022@users.noreply.github.com>
2022-08-24 10:35:54 +0200

    Create actions.py

---
+++ actions.py
@@ -0,0 +1,111 @@
+#
+#Actions.py
+ # This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+#This is a simple example for a custom action which utters "Hello World!"
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import sqlite3
+connection=sqlite3.connect("C:\sqlite\e_commerce.db")
+cursor= connection.cursor()
+req=cursor.execute('SELECT * FROM  data_cleaned')
+print(req.fetchall())
+connection.close()
+
+class ActionCommandeProduct(Action):
+
+    def name(self) -> Text:
+        return "action_pass_commande"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+# """ This files contains your custom actions which can be used to run
+# custom Python code.
+
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!" """
+
+
+# TODO: 
+# - query more than one field/column at a time
+# - if nothing matches both fields return results that match
+#   at least one
+# - data validation for close but not exact matches
+
+
+# multifield query process:
+# - first match each slot to closest item in that column
+# actual complex queries
+# - look for either, find union, 
+# if none inform not perfect match
+
+
